6299b80008bf60b7bdc4777b38e4ad8d2f7f495c
Vivekpatidar14 <herevivekpatidar@gmail.com>
2023-07-30 16:18:19 +0530

    my palindrome

---
+++ checkopalindrome.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <cctype>
+using namespace std;
+
+bool palindrome(string st)
+{
+    int s = 0;
+    int e = st.size() - 1;
+
+    while (s <= e)
+    {
+        int ch1 = st[s];
+        int ch2 = st[e];
+
+        if (!isalnum(ch1))
+        {
+            s++;
+            continue;
+        }
+        if (!isalnum(ch2))
+        {
+            e--;
+            continue;
+        }
+
+        if (ch1 == ch2)
+        {
+
+            s++;
+            e--;
+            return true;
+        }
+
+        return false;
+    }
+}
+int main()
+{
+
+    string s = "noo";
+
+    int x = palindrome(s);
+    if (x)
+    {
+        cout << "it is palindrome" << endl;
+    }
+    else
+    {
+        cout << "not palindrome" << endl;
+    }
+
+    return 0;
+}
